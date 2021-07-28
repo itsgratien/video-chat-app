c47fce90f703bdbf65019a020d8342da16c0da94
itsgracian <gracian2020@gmail.com>
2021-07-28 13:27:21 +0200

    design new UserDetail component

--- client/src/components/Reusable/OnlineUsers/OnlineUsers.tsx
+++ client/src/components/Reusable/OnlineUsers/OnlineUsers.tsx
@@ -1,28 +1,34 @@
 import React, { useState } from 'react';
 import './OnlineUsers.scss';
+import { UserDetail } from './UserDetail';
 
 const OnlineUsers = () => {
   const [hovered, setHovered] = useState<boolean>(false);
 
   return (
     <div className='fixed onlineUsers w-full top-0'>
-      <ul className='flex items-center justify-center'>
-        <li
-          className='relative'
-          onMouseOver={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
-        >
-          <div className='item flex items-center justify-center text-white'>
-            g
-          </div>
-          <div className='online absolute bottom-0 right-0'></div>
-        </li>
-        <li className='relative'>
-          <div className='item flex items-center justify-center text-white'>
-            g
-          </div>
-          <div className='online absolute bottom-0 right-0'></div>
-        </li>
+      <ul
+        className='flex items-center justify-center'
+        onMouseLeave={() => setHovered(false)}
+      >
+        {hovered ? (
+          <UserDetail email='johndoe@gmail.com' id='' setHovered={setHovered} />
+        ) : (
+          <>
+            <li className='relative' onMouseOver={() => setHovered(true)}>
+              <div className='item flex items-center justify-center text-white'>
+                g
+              </div>
+              <div className='online absolute bottom-0 right-0'></div>
+            </li>
+            <li className='relative'>
+              <div className='item flex items-center justify-center text-white'>
+                g
+              </div>
+              <div className='online absolute bottom-0 right-0'></div>
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
---
+++ client/src/components/Reusable/OnlineUsers/UserDetail/UserDetail.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './UserDetail.scss';
+import { Call } from 'react-ionicons';
+
+interface Props {
+  email: string;
+  id: string;
+  setHovered: (value: boolean) => void;
+}
+
+const UserDetail = (props: Props) => {
+  const { email, setHovered } = props;
+
+  return (
+    <div
+      className='userDetail flex items-center w-full justify-between'
+      onMouseLeave={() => setHovered(true)}
+    >
+      <div className='detail flex items-center'>
+        <div className='handle flex items-center justify-center font-bold text-white'>
+          g
+        </div>
+        <div className='font-bold ml-2'>{email}</div>
+      </div>
+      <div className='callUser'>
+        <button
+          type='button'
+          className='outline-none focus:outline-none flex items-center justify-center'
+        >
+          <Call />
+        </button>
+      </div>
+    </div>
+  );
+};
+export default UserDetail;
